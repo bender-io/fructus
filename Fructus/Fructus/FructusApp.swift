d5b8ac4048116d91bd3ecee3f37a339bee9b0b55
Brian Hersh <bri.d.hersh@gmail.com>
2020-09-24 15:06:48 -0400

    init commit

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Brian Hersh on 9/24/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
